c341245151ac5181e5d51d8601382aadcd40e6e1
CarrotPeeler <jared24mc@gmail.com>
2023-06-04 16:30:07 -0400

    Masking and augmentation added to inference, may revert

--- slowfast/inference/inference.py
+++ slowfast/inference/inference.py
@@ -1,7 +1,9 @@
 import os
 import torch
 import decord
+import random
 import pandas as pd
+import numpy as np
 import slowfast.utils.checkpoint as cu
 from slowfast.models import build_model
 from slowfast.config.defaults import assert_and_infer_cfg
@@ -10,15 +12,27 @@ from glob import glob
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 from typing import List
+from slowfast.datasets import utils as utils
+from slowfast.datasets.random_erasing import RandomErasing
+from slowfast.datasets.transform import (
+    MaskingGenerator,
+    MaskingGenerator3D,
+    create_random_augment,
+)
 
 
 class VideoProposalDataset(torch.utils.data.Dataset):
-    def __init__(self, video_path, frame_length, frame_stride, proposal_stride, transform=None, num_workers=1):
+    def __init__(self, cfg, video_path, frame_length, frame_stride, proposal_stride, num_workers=1):
+        self.cfg = cfg
+        self.aug = False
+        self.rand_erase = False
+
         self.video_path = video_path
         self.frame_length = frame_length
         self.transform = transform
         self.proposals = []
-        
+
+        # decode video and start proposal generation        
         frames = decord.VideoReader(self.video_path, num_threads=num_workers)
         proposal_length = frame_length * frame_stride
 
@@ -53,21 +67,149 @@ class VideoProposalDataset(torch.utils.data.Dataset):
         
         sampled_frames = torch.index_select(torch.from_numpy(frames_batch), 0, idxs)
         return sampled_frames
+    
+    """
+    Masking generation for frames
+    """
+    def _gen_mask(self):
+        if self.cfg.AUG.MASK_TUBE:
+            num_masking_patches = round(
+                np.prod(self.cfg.AUG.MASK_WINDOW_SIZE) * self.cfg.AUG.MASK_RATIO
+            )
+            min_mask = num_masking_patches // 5
+            masked_position_generator = MaskingGenerator(
+                mask_window_size=self.cfg.AUG.MASK_WINDOW_SIZE,
+                num_masking_patches=num_masking_patches,
+                max_num_patches=None,
+                min_num_patches=min_mask,
+            )
+            mask = masked_position_generator()
+            mask = np.tile(mask, (8, 1, 1))
+        elif self.cfg.AUG.MASK_FRAMES:
+            mask = np.zeros(shape=self.cfg.AUG.MASK_WINDOW_SIZE, dtype=np.int)
+            n_mask = round(
+                self.cfg.AUG.MASK_WINDOW_SIZE[0] * self.cfg.AUG.MASK_RATIO
+            )
+            mask_t_ind = random.sample(
+                range(0, self.cfg.AUG.MASK_WINDOW_SIZE[0]), n_mask
+            )
+            mask[mask_t_ind, :, :] += 1
+        else:
+            num_masking_patches = round(
+                np.prod(self.cfg.AUG.MASK_WINDOW_SIZE) * self.cfg.AUG.MASK_RATIO
+            )
+            max_mask = np.prod(self.cfg.AUG.MASK_WINDOW_SIZE[1:])
+            min_mask = max_mask // 5
+            masked_position_generator = MaskingGenerator3D(
+                mask_window_size=self.cfg.AUG.MASK_WINDOW_SIZE,
+                num_masking_patches=num_masking_patches,
+                max_num_patches=max_mask,
+                min_num_patches=min_mask,
+            )
+            mask = masked_position_generator()
+        return mask
         
     def __len__(self):
         "Returns the total number of proposals for this video."
         return len(self.proposals)
 
-    # Returns tuple w/ img tensor and class: (torch.Tensor, int)
+    # Code below mostly adapted from slowfast/slowfast/datasets/kinetics.py
     def __getitem__(self, index: int):
         "Returns one proposal (frames, start_frame_idx, end_frame_idx)."
+
+        # spatial_sample_index is in [0, 1, 2]. Corresponding to left,
+        # center, or right if width is larger than height, and top, middle,
+        # or bottom if height is larger than width.
+        spatial_sample_index = 1
+
+        min_scale, max_scale, crop_size = (
+            [self.cfg.DATA.TEST_CROP_SIZE] * 3
+            if self.cfg.TEST.NUM_SPATIAL_CROPS > 1
+            else [self.cfg.DATA.TRAIN_JITTER_SCALES[0]] * 2
+            + [self.cfg.DATA.TEST_CROP_SIZE]
+        )
+        min_scale, max_scale, crop_size = [min_scale], [max_scale], [crop_size]\
+        
+        # get proposal (frames, start_frame_idx, end_frame_idx)
         proposal = self.proposals[index]
+        frames_decoded = proposal[0]
+
+        num_decode = 1 # num of spatial crops
+        num_aug = 1
+        num_out = num_aug * num_decode
+        
+        f_out = [None] * num_out
+        idx = -1
+
+        for i in range(num_decode):
+            for _ in range(num_aug):
+                idx += 1
+                f_out[idx] = frames_decoded[i].clone()
+                f_out[idx] = f_out[idx].float()
+                f_out[idx] = f_out[idx] / 255.0
+
+                if self.aug and self.cfg.AUG.AA_TYPE:
+                    aug_transform = create_random_augment(
+                        input_size=(f_out[idx].size(1), f_out[idx].size(2)),
+                        auto_augment=self.cfg.AUG.AA_TYPE,
+                        interpolation=self.cfg.AUG.INTERPOLATION,
+                    )
+                    # T H W C -> T C H W.
+                    f_out[idx] = f_out[idx].permute(0, 3, 1, 2)
+                    list_img = self._frame_to_list_img(f_out[idx])
+                    list_img = aug_transform(list_img)
+                    f_out[idx] = self._list_img_to_frames(list_img)
+                    f_out[idx] = f_out[idx].permute(0, 2, 3, 1)
+
+                # Perform color normalization.
+                f_out[idx] = utils.tensor_normalize(
+                    f_out[idx], self.cfg.DATA.MEAN, self.cfg.DATA.STD
+                )
+
+                # T H W C -> C T H W.
+                f_out[idx] = f_out[idx].permute(3, 0, 1, 2)
+
+                scl, asp = (
+                    self.cfg.DATA.TRAIN_JITTER_SCALES_RELATIVE,
+                    self.cfg.DATA.TRAIN_JITTER_ASPECT_RELATIVE,
+                )
+                relative_scales = None
+                relative_aspect = None
+                
+                f_out[idx] = utils.spatial_sampling(
+                    f_out[idx],
+                    spatial_idx=spatial_sample_index,
+                    min_scale=min_scale[i],
+                    max_scale=max_scale[i],
+                    crop_size=crop_size[i],
+                    random_horizontal_flip=self.cfg.DATA.RANDOM_FLIP,
+                    inverse_uniform_sampling=self.cfg.DATA.INV_UNIFORM_SAMPLE,
+                    aspect_ratio=relative_aspect,
+                    scale=relative_scales,
+                    motion_shift=False,
+                )
+
+                if self.rand_erase:
+                    erase_transform = RandomErasing(
+                        self.cfg.AUG.RE_PROB,
+                        mode=self.cfg.AUG.RE_MODE,
+                        max_count=self.cfg.AUG.RE_COUNT,
+                        num_splits=self.cfg.AUG.RE_COUNT,
+                        device="cpu",
+                    )
+                    f_out[idx] = erase_transform(
+                        f_out[idx].permute(1, 0, 2, 3)
+                    ).permute(1, 0, 2, 3)
+
+                f_out[idx] = utils.pack_pathway_output(self.cfg, f_out[idx])
+                if self.cfg.AUG.GEN_MASK_LOADER:
+                    mask = self._gen_mask()
+                    f_out[idx] = f_out[idx] + [torch.Tensor(), mask]
+
+        frames = f_out[0] if num_out == 1 else f_out
     
-        # perform transform on frames if specified
-        if self.transform:
-            return (list(map(self.transform, proposal[0])), proposal[1], proposal[2])
-        else:
-            return proposal
+        # return proposal tuple with new frame modifications
+        return (frames, proposal[1], proposal[2])
         
 
 
@@ -129,7 +271,7 @@ if __name__ == '__main__':
     print(f"Number of threads: {num_workers}")
 
     for i in tqdm(range(len(video_paths))):
-        proposals_dataset = VideoProposalDataset(video_paths[i], frame_length, frame_stride, proposal_stride, transform, num_workers=num_workers)
+        proposals_dataset = VideoProposalDataset(cfg, video_paths[i], frame_length, frame_stride, proposal_stride, num_workers=num_workers)
         proposals_dataloader = DataLoader(dataset=proposals_dataset, batch_size=batch_size, num_workers=num_workers)
 
         video_name = video_paths[i].rpartition('/')[2]
