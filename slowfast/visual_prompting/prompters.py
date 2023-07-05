1f04e1ddef6848d46d5f8723abcb65cbf962ab2c
CarrotPeeler <jared24mc@gmail.com>
2023-07-05 14:42:01 -0400

    finished basic round 1 training for fixed patch and padding

--- slowfast/visual_prompting/prompters.py
+++ slowfast/visual_prompting/prompters.py
@@ -49,6 +49,7 @@ class PadPrompter(nn.Module):
         prompt = torch.cat(x.size(0) * [prompt], dim=0)
         
         return [x + prompt] # pyslowfast models expect list of tensors as input
+        
 
 
 class FixedPatchPrompter(nn.Module):
@@ -224,107 +225,55 @@ class NoiseCropPrompter(nn.Module):
         return [x + prompt]
     
 
-class Self_Attn(nn.Module):
-    """ Self attention Layer -- Adapted from SAGAN Paper https://arxiv.org/pdf/1805.08318.pdf """
-    def __init__(self, in_dim, activation):
-        super(Self_Attn,self).__init__()
-        self.chanel_in = in_dim
-        self.activation = activation
-        
-        self.query_conv = nn.Conv2d(in_channels = in_dim , out_channels = 32 , kernel_size= 1)
-        self.key_conv = nn.Conv2d(in_channels = in_dim , out_channels = 32 , kernel_size= 1)
-        self.value_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
-        self.gamma = nn.Parameter(torch.zeros(1))
-
-        self.softmax  = nn.Softmax(dim=-1) #
-        
-    def forward(self,x):
-        """
-            inputs :
-                x : input feature maps( B X C X W X H)
-            returns :
-                out : self attention value + input feature 
-                attention: B X N X N (N is Width*Height)
-        """
-        m_batchsize, C, width, height = x.size()
-        proj_query  = self.query_conv(x).view(m_batchsize,-1,width*height).permute(0,2,1) # B X C X (N)
-        proj_key =  self.key_conv(x).view(m_batchsize,-1,width*height) # B X C x (*W*H)
-        energy =  torch.bmm(proj_query,proj_key) # transpose check
-        attention = self.softmax(energy) # BX (N) X (N) 
-        proj_value = self.value_conv(x).view(m_batchsize,-1,width*height) # B X C X N
-
-        out = torch.bmm(proj_value,attention.permute(0,2,1) )
-        out = out.view(m_batchsize,C,width,height)
-        
-        out = self.gamma*out + x
-
-        return out, attention
-    
-
-class Attn(nn.Module):
+class MultiCamPadPrompter(nn.Module):
     def __init__(self, args):
-        super(Attn, self).__init__()
+        super(PadPrompter, self).__init__()
+        pad_size = args.PROMPT.PROMPT_SIZE if isinstance(args, CfgNode) else args.prompt_size
+        image_size = args.DATA.TRAIN_CROP_SIZE if isinstance(args, CfgNode) else args.image_size
+
+        self.base_size = image_size - pad_size*2
+
+        self.pad_up = nn.ParameterDict({
+            'Dashboard': nn.Parameter(torch.randn([3, 1, pad_size, image_size])),
+            'Right_side_window': nn.Parameter(torch.randn([3, 1, pad_size, image_size])),
+            'Rear_view': nn.Parameter(torch.randn([3, 1, pad_size, image_size]))  
+        })
+        self.pad_down = nn.ParameterDict({
+            'Dashboard': nn.Parameter(torch.randn([3, 1, pad_size, image_size])),
+            'Right_side_window': nn.Parameter(torch.randn([3, 1, pad_size, image_size])),
+            'Rear_view': nn.Parameter(torch.randn([3, 1, pad_size, image_size]))  
+        })
+        self.pad_left = nn.ParameterDict({
+            'Dashboard': nn.Parameter(torch.randn([3, 1, image_size - pad_size*2, pad_size])),
+            'Right_side_window': nn.Parameter(torch.randn([3, 1, image_size - pad_size*2, pad_size])),
+            'Rear_view': nn.Parameter(torch.randn([3, 1, image_size - pad_size*2, pad_size]))  
+        })
+        self.pad_right = nn.ParameterDict({
+            'Dashboard': nn.Parameter(torch.randn([3, 1, image_size - pad_size*2, pad_size])),
+            'Right_side_window': nn.Parameter(torch.randn([3, 1, image_size - pad_size*2, pad_size])),
+            'Rear_view': nn.Parameter(torch.randn([3, 1, image_size - pad_size*2, pad_size]))  
+        })
+
+    def forward(self, x, cam_views):
+        assert x.shape[0] == len(cam_views), \
+            f"len of cam_views does not match batch size of x; expected {x.shape[0]}, got {len(cam_views)} instead"
         
-        out_dim = 64
+        clip_prompts = []
 
-        self.l1 = nn.Sequential(
-            nn.Conv2d(3, out_dim, 4, 2, 1),
-            nn.LeakyReLU(0.1)
-        )
+        base = torch.zeros(3, 1, self.crop_size, self.crop_size).cuda()
 
-        self.l2 = nn.Sequential(
-            nn.Conv2d(out_dim, out_dim*2, 4, 2, 1),
-            nn.LeakyReLU(0.1)
-        )
+        for clip_idx in range(x.shape[0]):
+            cam_view = cam_views[clip_idx]
 
-        out_dim *= 2
+            clip_prompt = torch.cat([self.pad_left[cam_view], base, self.pad_right[cam_view]], dim=3)
+            clip_prompt = torch.cat([self.pad_up[cam_view], clip_prompt, self.pad_down[cam_view]], dim=2)
+            clip_prompt = torch.cat(x.size(2) * [clip_prompt], dim=1).unsqueeze(dim=0)
 
-        self.l3 = nn.Sequential(
-            nn.Conv2d(out_dim, out_dim*2, 4, 2, 1),
-            nn.LeakyReLU(0.1)
-        )
+            clip_prompts.append(clip_prompt)
 
-        out_dim *= 2
-
-        self.self_attn_1 = Self_Attn(out_dim, 'relu')
-
-        self.l4 = nn.Sequential(
-            nn.Conv2d(out_dim, out_dim*2, 4, 2, 1),
-            nn.LeakyReLU(0.1)
-        )
-
-        out_dim *= 2
-
-        self.self_attn_2 = Self_Attn(out_dim, 'relu')
-
-        self.last = nn.Sequential(
-            nn.Conv2d(out_dim, 3, 4)
-        )
-
-    def forward(self, x):
-        assert x.shape[3] == x.shape[4], f"input x does not have matching height and width dimensions; got ({x.shape[3]}, {x.shape[4]})"
-        # assert x.shape[0] == len(cam_views), f"len of cam_views does not match batch size of x; expected {x.shape[0]}, got {len(cam_views)} instead"
-
-        x = x.permute(0, 2, 1, 4, 3) # (B X C X T X H X W) => (B X T X C X W X H) to match Self_Attn input dims
-
-        prompt_clips = []
-
-        for clip in x: # => (T X C X W X H) => wherever dim B appears below its actually dim T
-            out = self.l1(clip)
-            out = self.l2(out)
-            out = self.l3(out)
-            out, attn_1 = self.self_attn_1(out)
-            out = self.l4(out)
-            out, attn_2 = self.self_attn_2(out)
-            out = self.last(out).unsqueeze(dim=0)
-
-            prompt_clips.append(out)
-
-        prompt = torch.cat(prompt_clips, dim=0)
-        prompt = prompt.permute(0, 2, 1, 4, 3)
-
-        return [prompt]
-
+        prompt = torch.cat(clip_prompts, dim=0)
+        
+        return [x + prompt] # pyslowfast models expect list of tensors as input
 
 
 def padding(args):
@@ -346,6 +295,5 @@ def crop(args):
 def noise_crop(args):
     return NoiseCropPrompter(args)
 
-
-def attn(args):
-    return Attn(args) 
+def mult_cam_padding(args):
+    return MultiCamPadPrompter(args)
