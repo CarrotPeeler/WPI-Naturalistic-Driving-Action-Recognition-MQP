464c4127e62092979276396203b6a3064d4d7a6c
CarrotPeeler <jared24mc@gmail.com>
2023-07-19 19:47:15 -0400

    optimizing tal code

--- slowfast/tools/test_net.py
+++ slowfast/tools/test_net.py
@@ -19,7 +19,6 @@ from slowfast.datasets import loader
 from slowfast.models import build_model
 from slowfast.utils.env import pathmgr
 from slowfast.utils.meters import AVAMeter, TestMeter
-from slowfast.datasets.decoder import temporal_sampling
 from visual_prompting import prompters
 
 logger = logging.get_logger(__name__)
@@ -59,9 +58,23 @@ def perform_test(test_loader, model, test_meter, cfg, writer=None, prompter=None
         print("DELETING predictions.txt")
         os.remove(pred_output)
 
-    activity_ids = []
-    localization_tuples = []
-    next_iter = 0
+
+    if cfg.TAL.ENABLE == True:
+        activity_ids = []
+        localization_tuples = []
+        next_iter = 0
+
+        start_time = 0
+        end_time = 0
+
+        prev_agg_pred = -1
+
+        # stores aggregated clips (batches) for each cam view type
+        cam_view_clips = {}
+
+        clip_agg_cnt = 0
+
+        
 
     for cur_iter, (inputs, labels, video_idx, time, meta, proposal) in enumerate(
         test_loader
@@ -150,121 +163,132 @@ def perform_test(test_loader, model, test_meter, cfg, writer=None, prompter=None
         ######################## T A L ########################
 
         elif cfg.TAL.ENABLE == True and cfg.TEST.BATCH_SIZE == 3:
-            cam_views = set()
-
-            # stores aggregated clips (batches) for each cam view type
-            cam_view_clips = {}
-                    
-            # check that there's no duplicate cam views and video ids + start/end times are matching
-            # also intialize cam view clips for storing aggregated clips 
-            for i in range(cfg.TEST.BATCH_SIZE):
-                cam_view = test_loader.dataset._path_to_videos[video_idx[i]].rpartition('/')[-1].partition('_user')[0] 
-                cam_views.add(cam_view)
                 
-                cam_view_clips[cam_view] = inputs[0][i].permute(1,0,2,3)
-
-            assert len(cam_views) == 3, f"Cam view mismatch for batch {cur_iter}"
-            assert len(set(proposal[0])) == len(set(proposal[1])) == len(set(proposal[2])) == 1, f"Proposal mismatch for batch {cur_iter}"
-
-            logger.info(f"CUR ITER: {cur_iter}")
-            
-            while cur_iter == next_iter:
-                
-                all_cam_view_preds = []
-                all_cam_view_probs = []
-
-                for cam_view_type in cam_view_clips.keys():
-
-                    if cam_view_clips[cam_view_type].shape[0] == cfg.DATA.NUM_FRAMES:
-                        input = [cam_view_clips[cam_view_type].permute(1,0,2,3).unsqueeze(dim=0)]
-                    
-                    else:
-                        start_idx = 0
-                        end_idx = start_idx + cfg.DATA.NUM_FRAMES - 1
-                        sampled = temporal_sampling(cam_view_clips[cam_view_type], start_idx, end_idx, cfg.DATA.NUM_FRAMES)
-                        input = [sampled.permute(1,0,2,3).unsqueeze(dim=0)]
-
-                    cam_view_preds = model(input).cpu()
-                    
-                    labels = labels.cpu()
-                    video_idx = video_idx.cpu()
-
-                    cam_view_pred = cam_view_preds.argmax().item()
-                    cam_view_prob = cam_view_preds.max().item()
-                    
-                    logger.info(f"batch: {next_iter}, {cam_view}, pred: {cam_view_pred}, prob: {cam_view_prob:.3f}")
-
-                    all_cam_view_preds.append(cam_view_pred)
-                    all_cam_view_probs.append(cam_view_prob)
-
-                preds = np.array(all_cam_view_preds)
-                probs = np.array(all_cam_view_probs)
-
-                # check if there is a common pred among candidates
-                if(stats.mode(preds, keepdims=False)[1] > 1):
-                    # cnt += 1
-                    # validate the probs of each pred are high enough to not be coincidence
-                    mode_pred = stats.mode(preds, keepdims=False)[0]
-
-                    mode_pred_idxs = np.where(preds == mode_pred)[0]
-                    minority_pred_idxs = np.where(preds != mode_pred)[0]
-
-                    mode_probs = np.array([probs[z] for z in mode_pred_idxs])
-                    minority_probs = np.array([probs[j] for j in minority_pred_idxs])
-
-                    # # check if minority pred exists and its prob is higher than mode probs
-                    # if(len(minority_pred_idxs) > 0 and minority_probs.max() > mode_probs.max() and mode_probs.max() < prob_threshold):
-                    #     # print(f"{preds} ==> {probs}")
-                    #     minority_pred = np.array([preds[m] for m in minority_pred_idxs]).max()
-                    #     agg_preds.append(minority_pred)
-                    #     agg_probs.append(minority_probs.max())
-                    
-                    # # select common pred if mean of common pred probs >= threshold
-                    # else:
-                    activity_ids.append(mode_pred)
-                    agg_prob = mode_probs.mean()
-                    logger.info(f"batch: {next_iter}, agg pred: {mode_pred}, agg prob: {agg_prob:.3f}")
-
-                # no common pred => select highest prob pred among all three camera angles
-                else:
-                    best_prob_idx = probs.argmax()
-                    activity_ids.append(preds[best_prob_idx])
-                    agg_prob = probs.max()
-                    logger.info(f"batch: {next_iter}, agg pred: {preds[best_prob_idx]}, agg prob: {agg_prob:.3f}")
-
-                localization_tuples.append((proposal[1][0], proposal[2][0]))
-
-                (inputs, labels, video_idx, time, meta, proposal) = next(iter(test_loader))
-
-                if cfg.NUM_GPUS:
-                    # Transfer the data to the current GPU device.
-                    if isinstance(inputs, (list,)):
-                        for i in range(len(inputs)):
-                            inputs[i] = inputs[i].cuda(non_blocking=True)
-                    else:
-                        inputs = inputs.cuda(non_blocking=True)
-                    # Transfer the data to the current GPU device.
-                labels = labels.cuda()
-                video_idx = video_idx.cuda()
-                next_iter += 1
+            if clip_agg_cnt == 0:
+                # perform first iter setup
                 cam_views = set()
                         
                 # check that there's no duplicate cam views and video ids + start/end times are matching
-                # Concat clips to their respective batch (based on cam view type) 
+                # also intialize cam view clips for storing aggregated clips 
                 for i in range(cfg.TEST.BATCH_SIZE):
-                    cam_view = test_loader.dataset._path_to_videos[video_idx[i]].rpartition('/')[-1].partition('_user')[0]
+                    cam_view = test_loader.dataset._path_to_videos[video_idx[i]].rpartition('/')[-1].partition('_user')[0] 
                     cam_views.add(cam_view)
+                    
+                    cam_view_clips[cam_view] = inputs[0][i].permute(1,0,2,3)
 
-                    cam_view_clips[cam_view] = torch.cat([cam_view_clips[cam_view], inputs[0][i].permute(1,0,2,3)], dim=0)
-                    assert cam_view_clips[cam_view].shape[1] == 3 \
-                        and cam_view_clips[cam_view].shape[2] == cfg.DATA.TEST_CROP_SIZE \
-                        and cam_view_clips[cam_view].shape[3] == cfg.DATA.TEST_CROP_SIZE, f"Shape mismatch, got {cam_view_clips[cam_view].shape}"
+                assert len(cam_views) == 3, f"Cam view mismatch for batch {cur_iter}"
+                assert len(set(proposal[0])) == len(set(proposal[1])) == len(set(proposal[2])) == 1, f"Proposal mismatch for batch {cur_iter}"
 
-                assert len(cam_views) == 3, f"Cam view mismatch for next batch {next_iter}"
+                start_time = proposal[1][0]
+                end_time = proposal[2][0]
+
+            else:
+                end_time = proposal[1][0]
+
+                cviews = set()
+                        
+                # check that there's no duplicate cam views and video ids + start/end times are matching
+                # Concat clips to their respective batch (based on cam view type) 
+                for j in range(cfg.TEST.BATCH_SIZE):
+                    cview = test_loader.dataset._path_to_videos[video_idx[j]].rpartition('/')[-1].partition('_user')[0]
+                    cviews.add(cview)
+
+                    cam_view_clips[cview] = torch.cat([cam_view_clips[cview], inputs[0][j].permute(1,0,2,3)], dim=0)
+                    assert cam_view_clips[cview].shape[1] == 3 \
+                        and cam_view_clips[cview].shape[2] == cfg.DATA.TEST_CROP_SIZE \
+                        and cam_view_clips[cview].shape[3] == cfg.DATA.TEST_CROP_SIZE, f"Shape mismatch, got {cam_view_clips[cview].shape}"
+
+                assert len(cviews) == 3, f"Cam view mismatch for next batch {next_iter}"
                 assert len(set(proposal[0])) == len(set(proposal[1])) == len(set(proposal[2])) == 1, f"Proposal mismatch for next batch {next_iter}"
 
-                if next_iter == 30:
-                    break
+            logger.info(f"CUR ITER: {cur_iter}")
+                    
+            all_cam_view_preds = []
+            all_cam_view_probs = []
+
+            for cam_view_type in cam_view_clips.keys():
+                if(cam_view_type == "Dashboard"):
+                    logger.info(f"CLIP AGG SHAPE: {cam_view_clips[cam_view_type].shape[0]}")
+
+                if cam_view_clips[cam_view_type].shape[0] == cfg.DATA.NUM_FRAMES:
+                    input = [cam_view_clips[cam_view_type].permute(1,0,2,3).unsqueeze(dim=0)]
+                
+                else:
+                    start_idx = 0
+                    end_idx = start_idx + cam_view_clips[cam_view_type].shape[0] - 1
+                    sampled = temporal_sampling(cam_view_clips[cam_view_type], start_idx, end_idx, cfg.DATA.NUM_FRAMES)
+                    input = [sampled.permute(1,0,2,3).unsqueeze(dim=0)]
+
+                cam_view_preds = model(input).cpu()
+                
+                labels = labels.cpu()
+                video_idx = video_idx.cpu()
+
+                cam_view_pred = cam_view_preds.argmax().item()
+                cam_view_prob = cam_view_preds.max().item()
+
+                # cam_view = test_loader.dataset._path_to_videos[video_idx[i]].rpartition('/')[-1].partition('_user')[0] 
+                # logger.info(f"batch: {next_iter}, {cam_view}, pred: {cam_view_pred}, prob: {cam_view_prob:.3f}")
+
+                all_cam_view_preds.append(cam_view_pred)
+                all_cam_view_probs.append(cam_view_prob)
+
+            preds = np.array(all_cam_view_preds)
+            probs = np.array(all_cam_view_probs)
+
+            # check if there is a common pred among candidates
+            if(stats.mode(preds, keepdims=False)[1] > 1):
+                # cnt += 1
+                # validate the probs of each pred are high enough to not be coincidence
+                mode_pred = stats.mode(preds, keepdims=False)[0]
+
+                mode_pred_idxs = np.where(preds == mode_pred)[0]
+                minority_pred_idxs = np.where(preds != mode_pred)[0]
+
+                mode_probs = np.array([probs[z] for z in mode_pred_idxs])
+                minority_probs = np.array([probs[j] for j in minority_pred_idxs])
+
+                # # check if minority pred exists and its prob is higher than mode probs
+                # if(len(minority_pred_idxs) > 0 and minority_probs.max() > mode_probs.max() and mode_probs.max() < prob_threshold):
+                #     # print(f"{preds} ==> {probs}")
+                #     minority_pred = np.array([preds[m] for m in minority_pred_idxs]).max()
+                #     agg_preds.append(minority_pred)
+                #     agg_probs.append(minority_probs.max())
+                
+                # # select common pred if mean of common pred probs >= threshold
+                # else:
+                curr_agg_pred = mode_pred
+                agg_prob = mode_probs.mean()
+                logger.info(f"batch: {next_iter}, agg pred: {curr_agg_pred}, agg prob: {agg_prob:.3f}")
+
+            # no common pred => select highest prob pred among all three camera angles
+            else:
+                best_prob_idx = probs.argmax()
+                curr_agg_pred = preds[best_prob_idx]
+                agg_prob = probs.max()
+                logger.info(f"batch: {next_iter}, agg pred: {curr_agg_pred}, agg prob: {agg_prob:.3f}")
+
+            # localization for this action segment is done
+            if curr_agg_pred != prev_agg_pred and clip_agg_cnt > 0:
+                logger.info(f"pred: {prev_agg_pred}, stamps: {(start_time, end_time)}")
+                activity_ids.append(prev_agg_pred)
+                localization_tuples.append((start_time, end_time))
+
+                # update previous prediction for next batch iter
+                prev_agg_pred = curr_agg_pred
+
+                # update start time
+                start_time = proposal[2][0]
+
+                # reset all clip aggregation variables
+                clip_agg_cnt = 0
+                cam_view_clips = {}
+            else:
+                # update previous prediction for next batch iter
+                prev_agg_pred = curr_agg_pred
+
+                # increment count for num clips concatenated consecutively
+                clip_agg_cnt += 1
 
         else:
             # Perform the forward pass.
@@ -449,29 +473,53 @@ def test(cfg):
 
     result_string_views = "_p{:.2f}_f{:.2f}".format(params / 1e6, flops)
 
-    for view, test_meter in zip(cfg.TEST.NUM_TEMPORAL_CLIPS, test_meters):
-        logger.info(
-            "Finalized testing with {} temporal clips and {} spatial crops".format(
-                view, cfg.TEST.NUM_SPATIAL_CROPS
+    if(cfg.TAL.ENABLE == False):
+        for view, test_meter in zip(cfg.TEST.NUM_TEMPORAL_CLIPS, test_meters):
+            logger.info(
+                "Finalized testing with {} temporal clips and {} spatial crops".format(
+                    view, cfg.TEST.NUM_SPATIAL_CROPS
+                )
             )
-        )
-        result_string_views += "_{}a{}" "".format(
-            view, test_meter.stats["top1_acc"]
-        )
-
-        result_string = (
-            "_p{:.2f}_f{:.2f}_{}a{} Top5 Acc: {} MEM: {:.2f} f: {:.4f}"
-            "".format(
-                params / 1e6,
-                flops,
-                view,
-                test_meter.stats["top1_acc"],
-                test_meter.stats["top5_acc"],
-                misc.gpu_mem_usage(),
-                flops,
+            result_string_views += "_{}a{}" "".format(
+                view, test_meter.stats["top1_acc"]
             )
-        )
 
-        logger.info("{}".format(result_string))
-    logger.info("{}".format(result_string_views))
+            result_string = (
+                "_p{:.2f}_f{:.2f}_{}a{} Top5 Acc: {} MEM: {:.2f} f: {:.4f}"
+                "".format(
+                    params / 1e6,
+                    flops,
+                    view,
+                    test_meter.stats["top1_acc"],
+                    test_meter.stats["top5_acc"],
+                    misc.gpu_mem_usage(),
+                    flops,
+                )
+            )
+
+            logger.info("{}".format(result_string))
+        logger.info("{}".format(result_string_views))
+    else:
+        return 
+    
     return result_string + " \n " + result_string_views
+
+
+def temporal_sampling(frames, start_idx, end_idx, num_samples):
+    """
+    Given the start and end frame index, sample num_samples frames between
+    the start and end with equal interval.
+    Args:
+        frames (tensor): a tensor of video frames, dimension is
+            `num video frames` x `channel` x `height` x `width`.
+        start_idx (int): the index of the start frame.
+        end_idx (int): the index of the end frame.
+        num_samples (int): number of frames to sample.
+    Returns:
+        frames (tersor): a tensor of temporal sampled video frames, dimension is
+            `num clip frames` x `channel` x `height` x `width`.
+    """
+    index = torch.linspace(start_idx, end_idx, num_samples)
+    index = torch.clamp(index, 0, frames.shape[0] - 1).long().to("cuda")
+    frames = torch.index_select(frames, 0, index)
+    return frames
