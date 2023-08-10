dadc89145092efa70f4e3c5f37018aacd55e81f6
CarrotPeeler <jared24mc@gmail.com>
2023-08-10 15:50:06 -0400

    graphs

--- slowfast/evaluation/graph_val_acc.py
+++ slowfast/evaluation/graph_val_acc.py
@@ -107,34 +107,38 @@ if __name__ == '__main__':
     json_paths = [
         # dir_path + 'prompted_multicam_padding/json_stats_mvitv2-b_round1_multicam_padding_30_lr_0.02_wd_1e-4_base-lr_9e-5_end-lr_1e-6_warmup-lr_1e-6.log',
         # dir_path + 'prompted_multicam_padding/selective_updating/json_stats_mvitv2-b_round1_selective_updating_multicam_padding_30_lr_0.02_wd_1e-4_base-lr_9e-5_end-lr_1e-6_warmup-lr_1e-6.log'
-        dir_path + 'normal_data/Json_stats_mvitv2-b_round1_unprompted_base-lr_2e-4_end-lr_2e-6_30epoch-warmup-lr_2e-6.log',
+        # dir_path + 'normal_data/Json_stats_mvitv2-b_round1_unprompted_base-lr_2e-4_end-lr_2e-6_30epoch-warmup-lr_2e-6.log',
         # dir_path + 'prompted_multicam_padding/v2/Json_stats_mvitv2-b_round1_selective_updating_multicam_paddingv2_30_lr_40_wd_1e-4_base-lr_2e-4_end-lr_2e-6_30epoch-warmup-lr_2e-6.log',
         # dir_path + 'prompted_multicam_padding/v2/Json_stats_mvitv2-b_round1_selective_updating_multicam_paddingv2_30_lr_40_wd_1e-4_base-lr_2e-4_end-lr_2e-6_30epoch-warmup-lr_2e-6_trial2.log',
         # dir_path + 'prompted_multicam_padding/v2/Json_stats_mvitv2-b_round1_selective_updating_multicam_paddingv2_30_lr_0.1_wd_1e-4_base-lr_2e-4_end-lr_2e-6_30epoch-warmup-lr_2e-6.log',
         # dir_path + 'prompted_padding/Json_stats_mvitv2-b_round1_padding_30_lr_0.05_wd_1e-4_base-lr_2e-4_end-lr_2e-6_30epoch-warmup-lr_2e-6.log',
         # dir_path + 'prompted_multicam_padding/Json_stats_mvitv2-b_round1_multicam_padding_30_lr_0.1_wd_1e-4_base-lr_2e-4_end-lr_2e-6_30epoch-warmup-lr_2e-6.log',
-        dir_path + 'prompted_multicam_padding/selective_updating/best_results/Json_stats_mvitv2-b_round1_selective_updating_multicam_padding_30_lr_0.1_wd_1e-4_base-lr_2e-4_end-lr_2e-6_30epoch-warmup-lr_2e-6.log',
-        dir_path + 'prompted_multicam_padding/selective_updating/best_results/Json_stats_mvitv2-b_round1_selective_updating_multicam_padding_30_lr_0.1_wd_1e-4_base-lr_2e-4_end-lr_2e-6_30epoch-warmup-lr_2e-6_pretrained_prompt_200epochs.log',
-        dir_path + 'prompted_multicam_padding/selective_updating/Json_stats_mvitv2-b_round1_selective_updating_multicam_padding_30_lr_0.1_wd_1e-4_base-lr_2e-4_end-lr_2e-6_30epoch-warmup-lr_2e-6_pretrained_prompt_400epochs.log',
+        # dir_path + 'prompted_multicam_padding/selective_updating/best_results/Json_stats_mvitv2-b_round1_selective_updating_multicam_padding_30_lr_0.1_wd_1e-4_base-lr_2e-4_end-lr_2e-6_30epoch-warmup-lr_2e-6.log',
+        # dir_path + 'prompted_multicam_padding/selective_updating/best_results/Json_stats_mvitv2-b_round1_selective_updating_multicam_padding_30_lr_0.1_wd_1e-4_base-lr_2e-4_end-lr_2e-6_30epoch-warmup-lr_2e-6_pretrained_prompt_200epochs.log',
+        # dir_path + 'prompted_multicam_padding/selective_updating/Json_stats_mvitv2-b_round1_selective_updating_multicam_padding_30_lr_0.1_wd_1e-4_base-lr_2e-4_end-lr_2e-6_30epoch-warmup-lr_2e-6_pretrained_prompt_400epochs.log',
         # dir_path + 'prompted_multicam_padding/selective_updating/best_results/json_stats_mvitv2-b_round1_selective_updating_multicam_padding_30_lr_0.02_wd_1e-4_base-lr_2e-4_end-lr_2e-6_30epoch-warmup-lr_2e-6.log'
+        dir_path + 'MVITv2_B_32x3_mixup_aug_unprompted/json_stats_MVITv2_B_32x3_mixup_aug_unprompted.log',
+        dir_path + 'MVITv2_B_32x3_mixup_aug_unprompted/json_stats_MVITv2_B_32x3_mixup_aug_unprompted_pretrained200.log',
     ]
 
     legend_labels = [
         # 'multicam padding\nprompt LR = 0.02',
         # 'selective updating, multicam padding\nprompt LR = 0.02'
-        'unprompted training',
+        # 'unprompted training',
         # 'selective updating, multicam padding v2\nprompt LR = 40',
         # 'selective updating, multicam padding v2\nprompt LR = 40, trial 2',
         # 'selective updating, multicam padding v2\nprompt LR = 0.1',
         # 'padding\nprompt LR = 0.05',
         # 'multicam padding\nprompt LR = 0.1',
-        'selective updating, multicam padding\nprompt LR = 0.1',
-        'selective updating, multicam padding\npre-trained prompt (200 epochs)',
-        'selective updating, multicam padding\npre-trained prompt (400 epochs)',
+        # 'selective updating, multicam padding\nprompt LR = 0.1',
+        # 'selective updating, multicam padding\npre-trained prompt (200 epochs)',
+        # 'selective updating, multicam padding\npre-trained prompt (400 epochs)',
         # 'selective updating, multicam padding\nprompt LR = 0.02'
+        'unprompted mixup/augment training, 200 epochs',
+        'unprompted mixup/augment training, 400 epochs',
     ]
 
-    figtitle = "Top1 Validation Accuracy Over Time for MViTv2-B\n(Base LR = 2e-4, start and end LR = 2e-6, 30 epoch warmup)"
+    figtitle = "Top1 Validation Accuracy Over Time for MViTv2-B\n(Base LR = 5e-4, start and end LR = 1e-6, 35 epoch warmup)"
     # figtitle = "Top1 Validation Accuracy Over Time for MViTv2-B\n(Base LR = 9e-5, start and end LR = 1e-6, 15 epoch warmup)"
 
-    graph_top1_val_acc(json_paths, num_epochs, legend_labels, figtitle, min_epoch_window=100, sliding_window=10, tracked_accs=[81, 82, 82.5, 83])
+    graph_top1_val_acc(json_paths, num_epochs, legend_labels, figtitle, min_epoch_window=100, sliding_window=10, tracked_accs=[83, 84, 85])
